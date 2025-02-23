a0c6b68b9ee98e60f2c6babcf1dcb67f90ba68a9
Amith <amithv0705@gmail.com>
2025-02-23 17:21:25 +0530

    Added transformation component

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecuritySystem
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataTransformationConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
-from networksecurity.components.data_validation import DataValidation,DataValidationConfig
+from networksecurity.components.data_validation import DataValidation
 ##To know what to import like above just check __init__ of that file and see what it is aslking/demanding to be executed
 
+from networksecurity.components.data_transformation import DataTransformation
 if __name__=="__main__":
     try:
         trainingPipelineConfig=TrainingPipelineConfig()
@@ -24,6 +26,12 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data validation completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingPipelineConfig)
+        logging.info("Initiating data transformation")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        logging.info("Initiating data transformation")
+        print(data_ingestion_artifact)
     except Exception as e:
         raise NetworkSecuritySystem(e,sys)
     
